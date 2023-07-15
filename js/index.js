adfd60c47a26675c581b0b6a3d22113c4492d0af
SeifElborolossy <123378393+SeifElborolossy@users.noreply.github.com>
2023-07-15 17:54:09 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,716 @@
+
+// ANIMATE ON SCROLL STARTS
+AOS.init();
+// ANIMATE ON SCROLL ENDS
+
+const items = [
+  {
+    title: "Tea",
+    img: "images/Hot Drinks/tea.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Teapot",
+    img: "images/Hot Drinks/teapot.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "12 L.E",
+  },
+  {
+    title: "Green Tea",
+    img: "images/Hot Drinks/green-tea.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "15 L.E",
+  },
+  {
+    title: "Flavored Tea",
+    img: "images/Hot Drinks/flavored-tea.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "18 L.E",
+  },
+  {
+    title: "Tea with Milk",
+    img: "images/Hot Drinks/milky-tea.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "18 L.E",
+  },
+  {
+    title: "Herbal Tea",
+    img: "images/Hot Drinks/herbal-tea.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "12 L.E",
+  },
+  {
+    title: "Herbal Cocktails",
+    img: "images/Hot Drinks/herbal-cocktail.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "18 L.E",
+  },
+  {
+    title: "Hot Cider",
+    img: "images/Hot Drinks/hot-cider.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Turkish Coffee",
+    img: "images/Hot Drinks/turkish-coffee.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "13 L.E, 18 L.E",
+  },
+  {
+    title: "French Coffee",
+    img: "images/Hot Drinks/french-coffee.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "18 L.E",
+  },
+  {
+    title: "Hazelnut Coffee",
+    img: "images/Hot Drinks/hazelnut-coffee.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Nutella Coffee",
+    img: "images/Hot Drinks/nutella-coffee.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "27 L.E",
+  },
+  {
+    title: "Espresso",
+    img: "images/Hot Drinks/espresso.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "22 L.E, 28 L.E",
+  },
+  {
+    title: "Cappuccino",
+    img: "images/Hot Drinks/cappuccino.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Macchiato",
+    img: "images/Hot Drinks/macchiato.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Latte",
+    img: "images/Hot Drinks/latte.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "28 L.E",
+  },
+  {
+    title: "Mocha",
+    img: "images/Hot Drinks/mocha.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Nescafe",
+    img: "images/Hot Drinks/nescafe.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Hot Chocolate",
+    img: "images/Hot Drinks/hot-chocolate.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "27 L.E",
+  },
+  {
+    title: "Nuts Salep",
+    img: "images/Hot Drinks/hazelnut-coffee.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Nutella Salep",
+    img: "images/Hot Drinks/nutella-coffee.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Fruits Salep",
+    img: "images/Hot Drinks/fruits.png",
+    category: "Hot Drinks",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Lemonade",
+    img: "images/Fresh Juices/lemonade.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "20 L.E",
+  },
+  {
+    title: "Mint Lemonade",
+    img: "images/Fresh Juices/mint-lemonade.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "22 L.E",
+  },
+  {
+    title: "Mango",
+    img: "images/Fresh Juices/mango.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Guava",
+    img: "images/Fresh Juices/guava.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Strawberry",
+    img: "images/Fresh Juices/strawberry.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Guava with Milk",
+    img: "images/Fresh Juices/guava.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Strawberry with Milk",
+    img: "images/Fresh Juices/strawberry.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Guava with Mint",
+    img: "images/Fresh Juices/guava.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Banana",
+    img: "images/Fresh Juices/banana.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Orange",
+    img: "images/Fresh Juices/orange.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Tangerine",
+    img: "images/Fresh Juices/tangerine.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Watermelon",
+    img: "images/Fresh Juices/watermelon.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Pineapple",
+    img: "images/Fresh Juices/pineapple.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Pomegranate",
+    img: "images/Fresh Juices/pomegranate.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Peach",
+    img: "images/Fresh Juices/peach.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Kiwi",
+    img: "images/Fresh Juices/kiwi.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Dates",
+    img: "images/Fresh Juices/dates.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Yogurt",
+    img: "images/Fresh Juices/yogurt.png",
+    category: "Fresh Juices",
+    rating: 4.9,
+    price: "25 L.E",
+  },
+  {
+    title: "Blueberry Milkshake",
+    img: "images/Milkshakes/blueberry.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Mango Milkshake",
+    img: "images/Milkshakes/mango.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "32 L.E",
+  },
+  {
+    title: "Strawberry Milkshake",
+    img: "images/Milkshakes/strawberry.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "32 L.E",
+  },
+  {
+    title: "Banana Milkshake",
+    img: "images/Milkshakes/banana.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "32 L.E",
+  },
+  {
+    title: "Nutella Milkshake",
+    img: "images/Milkshakes/nutella.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Vanilla Milkshake",
+    img: "images/Milkshakes/vanilla.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Hazelnut Milkshake",
+    img: "images/Milkshakes/hazelnut.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Oreo Milkshake",
+    img: "images/Milkshakes/oreo.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Hohos Milkshake",
+    img: "images/Milkshakes/hohos.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Twinkies Milkshake",
+    img: "images/Milkshakes/twinkies.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Kitkat Milkshake",
+    img: "images/Milkshakes/kitkat.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "50 L.E",
+  },
+  {
+    title: "Moro Milkshake",
+    img: "images/Milkshakes/moro.png",
+    category: "Milkshakes",
+    rating: 4.9,
+    price: "50 L.E",
+  },
+  {
+    title: "معسل سلوم",
+    img: "images/Hookah/m3sl.png",
+    category: "Hookah",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "معسل قص",
+    img: "images/Hookah/hookah mix.png",
+    category: "Hookah",
+    rating: 4.9,
+    price: "12 L.E",
+  },
+  {
+    title: "شيشة فواكه فاخر",
+    img: "images/Hookah/fruits.png",
+    category: "Hookah",
+    rating: 4.9,
+    price: "45 L.E",
+  },
+  {
+    title: "شيشة ميكس",
+    img: "images/Hookah/m3sl ks.png",
+    category: "Hookah",
+    rating: 4.9,
+    price: "50 L.E",
+  },
+  {
+    title: "Nutella",
+    img: "images/Hot Drinks/nutella-coffee.png",
+    category: "Extras",
+    rating: 4.9,
+    price: "12 L.E",
+  },
+  {
+    title: "Ice Cream",
+    img: "images/Fresh Juices/ice-cream.png",
+    category: "Extras",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Fruits",
+    img: "images/Hot Drinks/fruits.png",
+    category: "Extras",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Ice Mocha",
+    img: "images/Hot Drinks/mocha.png",
+    category: "Ice Coffee",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Ice Cappucino",
+    img: "images/Hot Drinks/cappuccino.png",
+    category: "Ice Coffee",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Ice Coffee",
+    img: "images/Hot Drinks/macchiato.png",
+    category: "Ice Coffee",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Ice Chocolate",
+    img: "images/Hot Drinks/hot-chocolate.png",
+    category: "Ice Coffee",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Ice Latte",
+    img: "images/Hot Drinks/latte.png",
+    category: "Ice Coffee",
+    rating: 4.9,
+    price: "10 L.E",
+  },
+  {
+    title: "Soda",
+    img: "images/Soft Drinks/soda.png",
+    category: "Soft Drinks",
+    rating: 4.9,
+    price: "20 L.E",
+  },
+  {
+    title: "Birell",
+    img: "images/Soft Drinks/birell.png",
+    category: "Soft Drinks",
+    rating: 4.9,
+    price: "24 L.E",
+  },
+  {
+    title: "Fayrouz",
+    img: "images/Soft Drinks/fayrouz.png",
+    category: "Soft Drinks",
+    rating: 4.9,
+    price: "24 L.E",
+  },
+  {
+    title: "Redbull",
+    img: "images/Soft Drinks/redbull.png",
+    category: "Soft Drinks",
+    rating: 4.9,
+    price: "40 L.E",
+  },
+  {
+    title: "Redbull Flavour",
+    img: "images/Soft Drinks/redbull-flavors.png",
+    category: "Soft Drinks",
+    rating: 4.9,
+    price: "45 L.E",
+  },
+  {
+    title: "Mineral Water",
+    img: "images/Soft Drinks/water-bottle.png",
+    category: "Soft Drinks",
+    rating: 4.9,
+    price: "6 L.E",
+  },
+  {
+    title: "Om Ali",
+    img: "images/Desserts/om-ali.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "40 L.E",
+  },
+  {
+    title: "Om Ali Nutella",
+    img: "images/Hot Drinks/nutella-coffee.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "50 L.E",
+  },
+  {
+    title: "Omi Ali Fruits",
+    img: "images/Hot Drinks/fruits.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "45 L.E",
+  },
+  {
+    title: "Milk Rice",
+    img: "images/Desserts/rice-milk.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "20 L.E",
+  },
+  {
+    title: "Ice Cream (3 scoops)",
+    img: "images/Desserts/ice-cream.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Fruit Salad",
+    img: "images/Desserts/fruit-salad.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Cheese Cake",
+    img: "images/Desserts/cheesecake.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "6 L.E",
+  },
+  {
+    title: "Fudge",
+    img: "images/Desserts/fudge.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "45 L.E",
+  },
+  {
+    title: "Molten Cake",
+    img: "images/Desserts/lava-cake.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "45 L.E",
+  },
+  {
+    title: "Brownies",
+    img: "images/Desserts/brownie.png",
+    category: "Desserts",
+    rating: 4.9,
+    price: "40 L.E",
+  },
+  {
+    title: "Sunshine",
+    img: "images/Cocktails/sunshine.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "28 L.E",
+  },
+  {
+    title: "Sunrise",
+    img: "images/Cocktails/sunrise.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Honey Yogurt",
+    img: "images/Cocktails/honey-yogurt.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Fruits Yogurt",
+    img: "images/Cocktails/fruits-yogurt.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Mango with Berries",
+    img: "images/Cocktails/mango-berries.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+    {
+    title: "Africano",
+    img: "images/Cocktails/africano.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Skyblue",
+    img: "images/Cocktails/sky-blue.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "40 L.E",
+  },
+  {
+    title: "Banana Nutella",
+    img: "images/Cocktails/banana-nutella.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Mango Kiwi",
+    img: "images/Cocktails/mango-kiwi.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Watermelon Mint",
+    img: "images/Cocktails/watermelon-mint.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "30 L.E",
+  },
+  {
+    title: "Oreo Mango",
+    img: "images/Cocktails/oreo-mango.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+  {
+    title: "Banana Caramel",
+    img: "images/Cocktails/banana-caramel.png",
+    category: "Cocktails",
+    rating: 4.9,
+    price: "35 L.E",
+  },
+
+];
+
+
+
+const menuItems = document.getElementById("menuItems");
+const hotDrinks = document.getElementById("hotDrinks");
+const freshJuices = document.getElementById("freshJuices");
+const milkshakes = document.getElementById("milkshakes");
+const softDrinks = document.getElementById("softDrinks");
+const cocktails = document.getElementById("cocktails");
+const hookah = document.getElementById("hookah");
+const extras = document.getElementById("extras");
+const iceCoffee = document.getElementById("iceCoffee");
+
+document.addEventListener('DOMContentLoaded', function() {
+ softDrinks.click();
+});
+
+function displayMenu(category) {
+  let cartona = ``;
+  menuItems.innerHTML = ``;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].category === category) {
+      cartona += `
+      <div class="col-lg-3">
+        <div class="card text-center hot-drinks">
+          <img src="${items[i].img}" class="img-fluid pt-3 mx-auto">
+          <div class="card-body">
+            <h5 class="card-title">${items[i].title}</h5>
+            <p class="card-text"><span class="fw-bold">Category:</span> ${items[i].category}</p>
+            <div class="details d-flex justify-content-between">
+              <div class="rating">
+                <i class="fa-solid fa-star"></i><span class="ps-1">${items[i].rating}</span>
+              </div>
+              <div class="price">
+                <i class="fa-solid fa-money-bill"></i><span class="ps-1">${items[i].price}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    }
+  }
+  menuItems.innerHTML = cartona;
+}
+
+const buttons = document.querySelectorAll(".menu-button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", function() {
+    // Remove styles from all buttons
+    buttons.forEach(btn => {
+      btn.style.backgroundColor = "";
+      btn.style.color = "";
+    });
+
+    // Set styles for the clicked button
+    button.style.backgroundColor = "white";
+    button.style.color = "#f4b937";
+
+    // Display the menu for the selected category
+    const category = button.textContent;
+    displayMenu(category);
+  });
+});
